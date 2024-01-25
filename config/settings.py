bc01e6b95eaf6be8b2fac36ea9d39c98f39ad439
AlirezaRezaie <rezaiealireza884@gmail.com>
2024-01-25 16:14:49 +0330

    chore: add postgres configs

--- config/settings.py
+++ config/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "technodb",
+        "USER": "technoadmin",
+        "PASSWORD": "admin1234",
+        "HOST": "localhost",  # or your host IP
+        "PORT": "5432",  # default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

aa0fa00333de379bd7a2337b8964ce41f993a243
AlirezaRezaie <rezaiealireza884@gmail.com>
2024-01-26 20:09:52 +0330

    fix: config staticfiles for ckeditor

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,9 @@ ALLOW_UNICODE_SLUGS = True
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "media")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

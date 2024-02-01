6f554498860dcbaf68ac0f8cf93cfa88381b1dce
AlirezaRezaie <rezaiealireza884@gmail.com>
2024-02-01 14:54:07 +0330

    fix: person list view

--- account/serializers.py
+++ account/serializers.py
@@ -1,16 +1,34 @@
 from rest_framework import serializers
-from .models import Person
+from .models import Person, Contact
+
 
 # Convert Unix time to Persian date
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        exclude = ("id", "contact_info")
 
 
 class PersonListSerializer(serializers.ModelSerializer):
     role = serializers.SerializerMethodField()
+    contact = serializers.SerializerMethodField()
 
     def get_role(self, obj):
         if obj.role:
             return obj.role.name
         else:
+            return None
+
+        # Your existing methods remain unchanged
+
+    def get_contact(self, obj):
+        try:
+            contact_instance = obj.contact
+            if contact_instance:
+                return ContactSerializer(contact_instance).data
+            else:
+                return None
+        except Contact.DoesNotExist:
             return None
 
     class Meta:
@@ -23,4 +41,5 @@ class PersonListSerializer(serializers.ModelSerializer):
             "profile_image",
             "about_me",
             "role",
+            "contact",
         )
